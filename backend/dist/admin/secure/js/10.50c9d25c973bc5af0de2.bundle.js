352a57f978170210f6ab5bfd5e42101568eb8041
Jim van de Ven <jimvandeven@sterrkdeta.nl>
2021-05-20 15:12:45 +0200

    build

---
+++ backend/dist/admin/secure/js/10.50c9d25c973bc5af0de2.bundle.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[10],{484:function(a,t,n){"use strict";n.r(t);var o=n(487),c=n(483);t.default=({data:a,field:{config:t}})=>a?t.format?Object(o.a)(Object(c.a)(a),t.format):a:null}}]);
--- backend/index.js
+++ backend/index.js
@@ -9,10 +9,18 @@ const EmployerSchema = require("./schemas/Employer.js");
 const dotenv = require("dotenv");
 dotenv.config();
 const PROJECT_NAME = "backend";
-const adapterConfig = { mongoUri: process.env.DATABASE_URL };
+const adapterConfig = {
+  mongoUri: process.env.DATABASE_URL,
+};
 
 const keystone = new Keystone({
   adapter: new Adapter(adapterConfig),
+  cookie: {
+    secure: process.env.NODE_ENV === "production", // Default to true in production
+    maxAge: 1000 * 60 * 60 * 24 * 30, // 30 days
+    sameSite: false,
+  },
+  cookieSecret: process.env.COOKIE_SECRET,
 });
 
 keystone.createList("Tag", TagSchema);
